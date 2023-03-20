6e56aad43bd82e72650ff4aaf0466bea30064bd6
h2m_kinoko <happy2mylife@gmail.com>
2023-03-20 10:43:56 +0900

    add footer.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,32 @@ import { BrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header";
 import { AuthProvider } from "./providers/AuthProvider";
 import { RouterConfig } from "./RouterConfig";
+import { Footer } from "./components/Footer";
 
 function App() {
+  const styleCss = {
+    minHeight: "100vw",
+    position: "relative",
+    paddingBottom: "30px",
+  } as const;
+
+  const Wrapper = ({ children }: any) => {
+    return (
+      <>
+        <div style={styleCss}>{children}</div>
+      </>
+    );
+  };
+
   return (
     <>
       <AuthProvider>
         <BrowserRouter>
-          <Header />
-          <RouterConfig />
+          <Wrapper>
+            <Header />
+            <RouterConfig />
+            <Footer />
+          </Wrapper>
         </BrowserRouter>
       </AuthProvider>
     </>
